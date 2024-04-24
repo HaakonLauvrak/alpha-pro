7c5594f68b57f72fc2dee02c4e595269ccfe3eda
haaklau <haaklau@stud.ntnu.no>
2024-04-24 11:06:44 +0200

    Fix typo and remove unused imports

--- main.py
+++ main.py
@@ -1,23 +1,3 @@
-from asyncio import sleep
-import copy
-import random
-import threading
-import time
-
-import numpy as np
-import pygame
-from actor.replay_buffer import REPLAY_BUFFER
-from game_logic.nim_state_manager import NIM_STATE_MANAGER
-from gui.hex_board import HEX_BOARD
-from gui.hex_game_gui import HEX_GAME_GUI
-from game_logic.hex_state_manager import HEX_STATE_MANAGER
-from gui.nim_board import NIM_BOARD
-from mcts.mcts import MCTSNode, MonteCarloTreeSearch
-from anytree import Node, RenderTree
-import config.config as config
-from actor.anet import ANET
-from tournaments.topp import Tournament
-import matplotlib.pyplot as plt
 from play.play import PLAY
 
 
--- tournaments/topp.py
+++ tournaments/topp.py
@@ -34,9 +34,9 @@ class Tournament():
                 current_state = self.make_new_game()
                 while not self.state_manager.isGameOver(current_state):
                     if current_state[1] == 1:
-                        move = self.state_manger.findMove(current_state, player1)
+                        move = self.state_manager.findMove(current_state, player1)
                     else:
-                        move = self.state_manger.findMove(current_state, player2)
+                        move = self.state_manager.findMove(current_state, player2)
                     self.state_manager.makeMove(move, current_state)
                 if self.state_manager.getReward(current_state) == -1:
                     results[player1.name] += 1
